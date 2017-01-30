110ad26f871d29fdc3fb2b4264d9219cc6f51a9e
Tony <yechao@umich.edu>
2017-01-29 22:25:52 -0500

    promisfy xml request and set up audio-buffer-loader

---
+++ web-audio-api/BufferLoader.js
@@ -0,0 +1,27 @@
+function BufferLoader(context, urls){
+	//return a new promise
+	let promiseArray = [];
+
+	for(let i=0; i<urls.length;i++){
+		let url = urls[i];	
+	  let promise = new Promise(function(resolve,reject){
+	  	let request = new XMLHttpRequest();
+	  	request.responseType = 'arraybuffer';
+	  	request.open('GET', url, true);
+			request.onload = function(){
+					if(this.status===200){
+						context.decodeAudioData(request.response,function(bufferData){
+						resolve(bufferData);
+						});
+					}else reject(Error(this.statusText));
+				}  
+			request.onerror = function(){
+				reject('Network Issue!');
+			};
+			request.send();
+	  });
+	  promiseArray.push(promise);
+	}
+
+	return Promise.all(promiseArray);
+}
